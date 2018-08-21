ac073cd7e481d3c354df6888a065313fe3dc4757
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 14:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-    if ( string == lowercase ){
+    if ( string === lowercase ){
   }
-   return "I can\'t hear you!";
+   return "I can\'t hear you!"
 }
 
 function sayHiToGrandma(string) {
-    if ( string == uppercase ) {
-    return "YES INDEED!";
+    if ( string === uppercase ) {
+    return "YES INDEED!"
   }
   
 }
 
-
 function sayHiToGrandma(string) {
   if( string === "I love you, Grandma.") {
-    return "I love you, too.";
+    return "I love you, too."
   }
+  
 }
