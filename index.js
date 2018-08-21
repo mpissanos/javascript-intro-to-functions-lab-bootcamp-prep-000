13fc21a1c73d2ecb5fcb16d26c50871459473a64
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 14:03:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
-logShout(string) {
-  console.log(string.toUpperCase())
+function logShout(string) {
+  console.log(string.toUpperCase());
 }
 
-log whisper(string) {
-  console.log(string.toLowerCase())
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if (string === lowercase){
+    return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(string) {
+  if( string === "I love you, Grandma.") {
+    return "I love you, too";
+  }
+}
+
