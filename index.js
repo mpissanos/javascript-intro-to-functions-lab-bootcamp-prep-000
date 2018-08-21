84fd53e17b43ee28ef107ae17523f30561a4ee14
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 14:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
     if ( string.toLowerCase() === lowercase ){
-   return "I can't hear you!"
+    return "I can't hear you!"
     }
     else if ( string.toUpperCase() === uppercase) {
     return "YES INDEED!"
